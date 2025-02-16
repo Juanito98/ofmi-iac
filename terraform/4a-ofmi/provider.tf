447c5002120ec53d46582a212d1e987a8381d178
Juan Carlos Sigler Priego <juanito@omegaup.com>
2025-02-16 16:18:27 -0500

    simpler structure (#3)
    * fix ci
    
    * test
    
    * add josito
    
    * add space
    
    * Replace comment
    
    * simpler structure

--- terraform/4a-ofmi/provider.tf
+++ terraform/4a-ofmi/provider.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    omegaup = {
+      source  = "Juanito98/omegaup"
+      version = "0.0.2"
+    }
+  }
   backend "s3" {
     bucket = "ofmi-iac"
     key    = "4a-ofmi/terraform.tfstate"
