999eb9348b8333a5367de91b10a520edbce67154
Juan Carlos Sigler Priego <juanito@omegaup.com>
2025-04-09 22:48:19 -0400

    Multiple providers (#15)
    * Multiple providers
    
    * hacerle caso al waring

--- terraform/4a-ofmi/secrets.tf
+++ terraform/4a-ofmi/secrets.tf
@@ -1,6 +1,11 @@
 # V2 contains the token for user Juan_Carlos_Sigler_Priego
-# every resource for omegaUp will be created on that account
-data "google_secret_manager_secret_version" "omegaup_api_key" {
+data "google_secret_manager_secret_version" "omegaup_api_key_juanito" {
   secret  = "OMEGAUP_API_KEY"
   version = 2
+}
+
+# V3 contains the token for user ofmi
+data "google_secret_manager_secret_version" "omegaup_api_key" {
+  secret  = "OMEGAUP_API_KEY"
+  version = 3
 }
