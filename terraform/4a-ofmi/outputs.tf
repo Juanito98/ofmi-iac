f74693f98a463d594db6779db74dd8a5d54ff433
Juan Carlos Sigler Priego <jcsiglerp@gmail.com>
2025-04-09 23:09:18 -0400

    Output identities link

--- terraform/4a-ofmi/outputs.tf
+++ terraform/4a-ofmi/outputs.tf
@@ -2,3 +2,8 @@ output "ofmi_2024_identities_gcs_link" {
   value       = module.ofmi_2024_identities.passwords_gcs_link
   description = "A url reference to the gcs file of the generated passwords."
 }
+
+output "ofmi_2024_selectivo_1_identities_gcs_link" {
+  value       = module.selectivo_1_identities.passwords_gcs_link
+  description = "A url reference to the gcs file of the generated passwords."
+}
